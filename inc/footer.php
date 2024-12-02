d4432bc1a6479a11a67480f6efef627cfccf1f0a
Singuila01 <mbayechris201@gmail.com>
2024-12-02 22:13:44 +0100

    Initialisation du projet

---
+++ inc/footer.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <footer>
+        <ul>
+            <li><a href="">HomePage</a></li>
+            <li><a href="">Legal notice</a></li>
+            <li>©2024</li>
+        </ul>
+    </footer>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="assets/css/styles.css">
+</head>
+<body>
+    <?php include_once 'inc/header.php' ?>
+
+    <section>
+        <div class="article">
+            <h1>Goal</h1>
+            <div class="zones presen">
+                <div class="zone">
+                    <p>Bienvenue sur notre jeu : <b>Unbeatable</b>.</p>
+                    <p>The goal of this game is easy. You need to play in group, reply properly at all questions and try to get as many points as possible to win the game.</p>
+                    <p>There are <b>tree</b> levels :</p>
+                    <ul>
+                        <li>Easy</li>
+                        <li>Medium</li>
+                        <li>Hard</li>
+                    </ul>
+                    <p>So, who will be ready to reply to the hardest questions to win ? Who'll be ready to take risks to win ? Let's see it and play !</p>
+                </div>
+                <div class="zone">
+                    <!-- <img src="https://m.media-amazon.com/images/I/71IZ7rqK0IL.jpg" alt=""> -->
+                </div>
+            </div>
+        </div>
+
+        <div class="article">
+            <h1>Rules</h1>
+            <div class="zones rules">
+                <div class="zone">
+                    <h3>1</h3>
+                    <p>At first, you need to create your create. Mix all people (bad people with strong people), and you're sure every group can win the game.</p>
+                </div>
+                <div class="zone">
+                    <h3>2</h3>
+                    <p>Choose your pawn. This pawn is your identy, your group and your opponent will know where are you in the set.</p>
+                </div>
+                <div class="zone">
+                    <h3>3</h3>
+                    <p>Every group needs to roll the dice one time, group after group, to know who'll start the game at first. The group who has the biggest number on the dice starts the game.</p>
+                </div>
+                <div class="zone">
+                    <h3>4</h3>
+                    <p>The group with the highest number on the dice have to choose the difficulty of their question: easy, medium, hard. The second group chooses a card that matches the difficulty.</p>
+                </div>
+                <div class="zone">
+                    <h3>5</h3>
+                    <p>The first group needs to answer. If they reply correctly, they advance their pawn on the set. The number of the points is the number of boxes to move forward on the set.</p>
+                </div>
+                <div class="zone">
+                    <h3>6</h3>
+                    <p>Each group have to reply to a questions, one after another, and try to have the most points.</p>
+                </div>
+                <div class="zone">
+                    <h3>7</h3>
+                    <p>The first group who crossed the finish line wins the game.</p>
+                </div>
+                
+            </div>
+        </div>
+
+        <div class="article">
+            <div class="zones formulaire">
+                <div class="form">
+                    <form action="" method="post">
+                        <input type="text" name="class" id="" placeholder="Your class">
+                        <textarea name="message" id="" placeholder="Your opinion"></textarea>
+                        <input type="submit" value="Send you opinion">
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <?php include_once 'inc/footer.php' ?>
+</body>
+</html>
